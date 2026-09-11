0b215ef3f223d8fd3e894605baa047f2711d652a
Nihal Hyder <contactnihaldxn@gmail.com>
2026-09-12 00:38:21 +0500

    uploading file for kali

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import socket
 
+def scan(ip, port_count):
+    # Convert port_count to int and add 1 to include the final port number
+    for p in range(1, int(port_count) + 1):
+        port_scan(ip, p)
 
+def port_scan(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)  
+    try:
+        s.connect((ip, port))  
+        print(f'[+] {port} port open')
+    except (socket.timeout, ConnectionRefusedError, OSError):
+        print(f'[-] {port} port closed')
+    finally:
+        s.close()  
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ips = input('Enter IP address(es) (separated by commas): ')
+port = input('Enter how many ports to scan: ')
 
-s.settimeout(1)
-try : 
-    s.connect(('192.168.54.101', 21))
-    print('[+] connection establish...')
-except:
-    print('[-] connection denied...')
-
-
-s.close()
+if ',' in ips:
+    print('[*] Scanning multiple IPs...')
+    for ip_address in ips.split(','):
+        scan(ip_address.strip(), port) 
+else:
+    print('[*] Scanning single IP...')
+    scan(ips.strip(), port)
